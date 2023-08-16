5ebc6bdd470cc62dc41ea5bd19cc6768df498934
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2023-08-16 15:25:04 -0300

    aula_19

--- aula_19_drawer/lib/main.dart
+++ aula_19_drawer/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  // Desclocar o MaterialApp para o runApp
+  // Isso permite o Navigator.pop()
+  runApp(const MaterialApp(home: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,11 +11,47 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Teste Drawer"),
+      ),
+      // Adicionar o drawer (menu sanduiche) ao Scaffold
+      drawer: Drawer(
+        // O ListView permite que o usuário possa rolar
+        // as opções no menu se não houver espaço vertical
+        // para mostrar todas as opções na mesma tela.
+        child: ListView(
+          // Remove o espaçamento entre os items da ListView
+          padding: EdgeInsets.zero,
+          children: [
+            // Cabeçalho
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("Cabeçalho"),
+            ),
+            // Lista de Opções do Menu
+            ListTile(
+              title: const Text("Primeira Opção..."),
+              onTap: () {
+                // Faz alguma coisa...
+                print("Opção 1 escolhida...");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Segunda Opção..."),
+              onTap: () {
+                // Faz alguma coisa...
+                print("Agora a segunda opção foi a escolhida...");
+              },
+            ),
+          ],
         ),
+      ),
+      body: const Center(
+        child: Text('Olá Mundo!!!'),
       ),
     );
   }
